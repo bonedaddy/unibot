a8c701b36e9a45fd7c13d11fcbf35838d5bba975
bonedaddy <boned4ddy@protonmail.com>
2021-01-06 01:09:09 -0800

    basic vet pass

--- bclient/types.go
+++ bclient/types.go
@@ -1,41 +1,28 @@
 package bclient
 
 import (
-	poolbindings "github.com/bonedaddy/unibot/bindings/pool"
 	"github.com/ethereum/go-ethereum/common"
 )
 
 var (
-	defi5Addr           = "0xfa6de2697d59e88ed7fc4dfe5a33dac43565ea41"
-	defi5StakeAddr      = "0x11bf850D1B85eA02eF9F06Cf09488E443655b586"
-	defi5UNILPStakeAddr = "0x04834fE50BE6954088d979A84bE20913efCA9118"
-	cc10Addr            = "0x17ac188e09a7890a1844e5e65471fe8b0ccfadf3"
+
+	// preset tokens
+
 	// DEFI5TokenAddress is the address of the DEFI5 token/pool contract
-	DEFI5TokenAddress = common.HexToAddress(defi5Addr)
-	// DEFI5StakingAddress is the address of the DEFI5 staking contract
-	DEFI5StakingAddress = common.HexToAddress(defi5StakeAddr)
-	// DEFI5UNILPStakingAddress is the address of the ETH-DEFI5 Uniswap staking contract
-	DEFI5UNILPStakingAddress = common.HexToAddress(defi5UNILPStakeAddr)
+	DEFI5TokenAddress = common.HexToAddress("0xfa6de2697d59e88ed7fc4dfe5a33dac43565ea41")
 	// CC10TokenAddress is the address of the CC10 token/pool contract
-	CC10TokenAddress = common.HexToAddress(cc10Addr)
+	CC10TokenAddress = common.HexToAddress("0x17ac188e09a7890a1844e5e65471fe8b0ccfadf3")
 	// WETHTokenAddress is the address of the WETH token contract
 	WETHTokenAddress = common.HexToAddress("0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2")
 	// DAITokenAddress is the address of the MCD (Multi Collateral DAI) contract
 	DAITokenAddress = common.HexToAddress("0x6b175474e89094c44da98b954eedeac495271d0f")
 	// NDXTokenAddress is the address of the NDX contract
 	NDXTokenAddress = common.HexToAddress("0x86772b1409b61c639eaac9ba0acfbb6e238e5f83")
-	// GovernorAlpha is the address of the GovernorALpha governance contract
-	GovernorAlpha = common.HexToAddress("0x95129751769f99CC39824a0793eF4933DD8Bb74B")
+
+	// misc variables
+
 	// InfuraWSURL is the URL for INFURA websockets access
 	InfuraWSURL = "wss://mainnet.infura.io/ws/v3/"
 	// InfuraHTTPURL is the URL for INFURA HTTP access
 	InfuraHTTPURL = "https://mainnet.infura.io/v3/"
-	// IndexPools contains all current available idnex pools
-	// note that it may not be truly up-to-date
-	IndexPools = map[string]common.Address{"defi5": DEFI5TokenAddress, "cc10": CC10TokenAddress}
 )
-
-// DEFI5 is the DEFI Top 5 IndexPool
-type DEFI5 struct {
-	*poolbindings.Poolbindings
-}
--- go.mod
+++ go.mod
@@ -1,3 +1,14 @@
 module github.com/bonedaddy/unibot
 
 go 1.15
+
+require (
+	github.com/bonedaddy/dgc v0.0.1
+	github.com/bwmarrin/discordgo v0.22.0
+	github.com/ethereum/go-ethereum v1.9.25
+	github.com/stretchr/testify v1.6.1
+	gopkg.in/yaml.v2 v2.4.0
+	gorm.io/driver/postgres v1.0.6
+	gorm.io/driver/sqlite v1.1.4
+	gorm.io/gorm v1.20.9
+)
